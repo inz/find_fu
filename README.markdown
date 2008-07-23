658ce4e17d203086ff3198999327e71b5212c868
Christian Inzinger <christian@blackwhale.at>
2008-07-23 15:45:10 +0200

    add r:if_url_with_match and r:match

--- README.markdown
+++ README.markdown
@@ -34,7 +34,23 @@ The date can be specified using the **`to`** parameter.
 
  * `r:children:new` as well as `r:children:old` support all regular options you can pass to `r:children[:...]`.
  * the `from` and `to` attributes are evaluated as pure ruby code, which could potentially be a **major security risk**! You have been warned! (Feel free to suggest alternatives though)
- 
+
+### if\_url\_with\_match
+
+> `<r:if_url_with_match matches="regexp">...</r:if_url_with_match>`
+
+Does the same as `<r:if_url>` except that every back-reference (as well as the whole matched string) you define in your regular expression will be accessible within the tag using `<r:match />`.
+
+### match
+
+> `<r:match [id="1"] />`
+
+Can be used within `r:if_url_with_match` to retrieve the value of any backreference defined in the regular expression.
+
+    <r:if_url_with_match matches="^/([^/])/.*$">
+      You're currently in the <r:match /> directory!
+    </r:if_url_with_match>
+
 ## Installation
 
 The extension is tested against 0.6.5, it should work for other versions though.
@@ -44,3 +60,9 @@ To install the extension just run
     git clone git://github.com/inz/find_fu.git vendor/extensions/find_fu
 
 and (re)start your radiant installation.
+
+Of course you can also include the extension as a git submodule like so:
+
+    git submodule add git://github.com/inz/find_fu.git vendor/extensions/find_fu
+    
+Enjoy!
--- app/models/find_fu_tags.rb
+++ app/models/find_fu_tags.rb
@@ -46,5 +46,43 @@ module FindFuTags
       result << tag.expand
     end 
     result
- end
+  end
+ 
+  desc %{
+   Renders the containing elements only if the page's url matches the regular expression
+   given in the @matches@ attribute. If the @ignore_case@ attribute is set to false, the
+   match is case sensitive. By default, @ignore_case@ is set to true.
+   
+   Additionally you will have access to any backreferences defined in the regular expression using @<r:match [id="0"] />@.
+ 
+   *Usage:*
+   <pre><code><r:if_url matches="regexp" [ignore_case="true|false"]>...</if_url></code></pre>
+  }
+  tag 'if_url_with_match' do |tag|
+   raise TagError.new("`if_url_with_match' tag must contain a `matches' attribute.") unless tag.attr.has_key?('matches')
+   regexp = build_regexp_for(tag, 'matches')
+   match = tag.locals.page.url.match(regexp)
+   unless match.nil?
+     tag.locals.match = match
+     tag.expand
+   end
+  end
+ 
+  desc %{
+   Renders a field matched by the parent @<r:if_url ...>@ regular expression.
+ 
+   You can optionally specify the @id@ of the matched field/backreference 
+   (defaults to the first matched field).
+ 
+   *Usage*:
+   <pre><code><r:match [id="1"] /></code></pre>
+  }
+  tag 'match' do |tag|
+    raise TagError.new("`match' tag must be child of `if_url_with_match' tag.") unless tag.locals.match
+    match = tag.locals.match
+    id = (tag.attr["id"] || 1).to_i
+    logger.info ">>> MATCH: #{match[id]}"
+    match[id]
+  end
+
 end
